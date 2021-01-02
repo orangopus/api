d58dd966948ebe0d1f2efbbfc2afa3873d48b0d9
Cheese <45247477+ChGGse@users.noreply.github.com>
2021-01-02 08:32:44 +0000

    Delete getData.js

--- pages/getData.js
+++
@@ -1,8 +0,0 @@
-import {table, minifyRecords} from './utils/Airtable'
-
-export default async (req, res) => {  
-    const records = await table.select({}).firstPage()
-    const minifiedRecords = minifyRecords(records)
-    res.statusCode = 200;
-    res.json(minifiedRecords)
-}
